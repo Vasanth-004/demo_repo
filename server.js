915409f9fa4106d004fda41b94abd01038067cc1
Vasanth-004 <vasanthmadishetty@gmail.com>
2025-06-09 14:53:03 +0530

    Hello 2

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const app = express();
 app.use(cors());
 
 app.get("/" , (req,res) => {
-    return res.send("Hello world!!");
+    return res.send("Hello world 2!!");
 });
 
 app.post("/webhook", (req,res)=> {
-    console.log(req.body);
+
+
+
+    return res.json(req.body);
 })
 
 app.listen(3000, () => {
